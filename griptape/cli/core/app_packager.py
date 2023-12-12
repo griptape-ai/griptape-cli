b231436bbcf86b25b952a94262888c06b4961b24
Zach Giordano <zgmx22@gmail.com>
2023-12-12 12:13:17 -0600

    Add app template gitignore with patterns that packager will ignore for deployments

--- griptape/cli/core/app_packager.py
+++ griptape/cli/core/app_packager.py
@@ -35,11 +35,17 @@ class AppPackager:
 
     def _create_deployment_tmp_dir(self) -> str:
         # Copy to new dir to ignore certain patterns
-        # TODO: Should more patterns be ignored?
+        ignore_patterns = []
+        if os.path.exists(os.path.join(self.app_directory, ".gitignore")):
+            with open(os.path.join(self.app_directory, ".gitignore"), "r") as file:
+                for line in file.readlines():
+                    if line.endswith("\n"):
+                        line = line[:-1]
+                    ignore_patterns.append(line.strip())
         return shutil.copytree(
             self.app_directory,
             os.path.join(self.app_directory, "zip_tmp"),
-            ignore=shutil.ignore_patterns(".venv*", "__pycache__*", "*.pyc"),
+            ignore=shutil.ignore_patterns(*ignore_patterns),
         )
 
     def _create_deployment_zip_file(self, tmp_dir: str) -> str:
