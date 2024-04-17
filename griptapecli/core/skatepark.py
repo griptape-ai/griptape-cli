c3dcba2c9598137b09c98bb3f4eca87c68cea35f
Collin Dutter <collin@griptape.ai>
2024-04-17 15:53:22 -0700

    Better error handling (#13)
    * Better error handling
    
    * Remove failed registrations

--- griptapecli/core/skatepark.py
+++ griptapecli/core/skatepark.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 
 from dotenv import dotenv_values
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from .models import Event, Run, Structure
 from .state import State, RunProcess
@@ -21,9 +21,14 @@ state = State()
 @app.post("/api/structures")
 def create_structure(structure: Structure) -> Structure:
     logger.info(f"Creating structure: {structure}")
-    state.register_structure(structure)
 
-    build_structure(structure.structure_id)
+    state.register_structure(structure)
+    try:
+        build_structure(structure.structure_id)
+    except HTTPException as e:
+        state.remove_structure(structure.structure_id)
+
+        raise HTTPException(status_code=400, detail=str(e))
 
     return structure
 
@@ -47,6 +52,9 @@ def build_structure(structure_id: str) -> Structure:
     logger.info(f"Building structure: {structure_id}")
     structure = state.get_structure(structure_id)
 
+    if not os.path.exists(structure.directory):
+        raise HTTPException(status_code=400, detail="Directory does not exist")
+
     structure.env = dotenv_values(f"{structure.directory}/.env")
 
     subprocess.call(
@@ -65,6 +73,12 @@ def build_structure(structure_id: str) -> Structure:
 def create_structure_run(structure_id: str, run: Run) -> Run:
     logger.info(f"Creating run for structure: {structure_id}")
     structure = state.get_structure(structure_id)
+
+    if not os.path.exists(structure.directory):
+        raise HTTPException(status_code=400, detail="Directory does not exist")
+
+    if not os.path.exists(f"{structure.directory}/{structure.main_file}"):
+        raise HTTPException(status_code=400, detail="Main file does not exist")
 
     process = subprocess.Popen(
         [".venv/bin/python3", structure.main_file, *run.args],
