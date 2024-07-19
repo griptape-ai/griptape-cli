c5409eda83bd3e8dd5a122a077a0892502fa0ca3
Collin Dutter <collindutter@gmail.com>
2024-07-18 21:01:48 -0700

    Mapping fixes

--- griptapecli/core/skatepark.py
+++ griptapecli/core/skatepark.py
@@ -113,10 +113,14 @@ embedding_driver_to_config_map: dict[type[BaseEmbeddingDriver], dict] = {
             region_name=os.environ["AWS_DEFAULT_REGION"],
         ),
     },
-    AzureOpenAiEmbeddingDriver: {
-        "api_key": os.environ["GOOGLE_API_KEY"],
-    },
     AmazonBedrockCohereEmbeddingDriver: {
+        "session": boto3.Session(
+            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+            region_name=os.environ["AWS_DEFAULT_REGION"],
+        ),
+    },
+    AzureOpenAiEmbeddingDriver: {
         "api_key": os.environ["AZURE_OPENAI_API_KEY"],
         "azure_endpoint": os.environ["AZURE_OPENAI_ENDPOINT"],
     },
